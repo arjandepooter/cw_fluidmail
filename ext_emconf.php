ae28bc0d3fcbc68d8fccc929ff64935c3ffcb19b
Arjan de Pooter <arjan@cmsworks.nl>
2014-07-23 09:11:04 +0200

    Updated emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -5,14 +5,27 @@ $EM_CONF[$_EXTKEY] = array(
 	'category' => 'misc',
 	'author' => 'Arjan de Pooter',
 	'author_email' => 'arjan@cmsworks.nl',
-	'state' => 'alpha',
+	'author_company' => 'CMS Works',
+	'shy' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'beta',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'version' => '0.0.1',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.2',
+			'typo3' => '6.2.0-6.2.99',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
 	),
+	'_md5_values_when_last_written' => 'a:0:{}',
 );
+?>
